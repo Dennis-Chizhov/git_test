6dbdc82af404e278adc886de1a8b62d93bace74e
TheWarlockVDS <elchiz1973@gmail.com>
2018-07-13 16:17:39 -0400

    Initial commit

---
+++ test.py
@@ -0,0 +1,4 @@
+word = str(input('What is your word? '))
+number = int(input('How many times do you want to repeat this? '))
+print ('The word', word, 'will be printed', number, 'of times.')
+print (word*number)
